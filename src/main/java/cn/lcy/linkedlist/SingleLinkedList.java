8c6e828dd7aa18fb834b7fb25c25aed8c60ff80a
qiyeyizhandeng <qiyeyizhandeng@myweimai.com>
2021-01-31 11:11:30 +0800

    回文字符串判断

--- src/main/java/cn/lcy/linkedlist/SingleLinkedList.java
+++ src/main/java/cn/lcy/linkedlist/SingleLinkedList.java
@@ -1,104 +1,102 @@
 package cn.lcy.linkedlist;
 
 
+import java.util.Map;
+
 public class SingleLinkedList {
 
-    public Node head;
+    private static Node head;
+
 
     public void insertToTail(String val) {
-        Node q = new Node(val, null);
+        Node p = new Node(val, null);
         if (head == null){
-            head = q;
+            head = p;
             return;
         }
-        Node p = head;
-        while (p.next != null) {
-            p = p.next;
-        }
-        p.next = q;
-    }
-
-
-    //判断是否回文字符串
-    public boolean rang() {
-        if (head == null || head.next == null) return false;
-        Node p = head;
         Node q = head;
-        //用快慢指针获取中间节点
-        while (q.next != null && q.next.next != null) {
-            p = p.next;
-            q = q.next.next;
+        while (q.next != null) {
+            q = q.next;
         }
-        Node right = null;
-        Node left = null;
-        if (q.next == null) {
-            System.out.println("链表为奇数节点");
-            right = p.next;
-            left = flip(p).next;
-        } else {
-            System.out.println("链表为偶数节点");
-            right = p.next;
-            left = flip(p);
-        }
-
-        return compareNodeList(left, right);
+        q.next = p;
     }
 
+    private static boolean compareNode(Node right, Node left) {
+        boolean flag= false;
+        while (right!=null && left!=null){
+            if (right.data.equals(left.data)){
+                right = right.next;
+                left = left.next;
+                flag = true;
+            }else {
+                break;
+            }
+        }
+        if (right!=null && left==null){
+            return false;
+        }
+        if (left!=null && right==null){
+            return false;
+        }
+        return flag;
+    }
 
-    //翻转链表
-    public Node flip(Node p) {
+    private static Node flip(Node p) {
         Node next = null;
         Node pre = null;
-        //要翻转的节点
         Node q = head;
-        while (q != p) {
-            //记录下一个要翻转的节点
+        while (q!=p){
             next = q.next;
-            //当前节点指向前置节点
             q.next = pre;
-            //记录前置节点
             pre = q;
-            //设置要翻转的节点
             q = next;
         }
-        //最后一个节点翻转
         p.next = pre;
         return p;
     }
 
-
-    //判断左右链表是否相等
-    public boolean compareNodeList(Node p, Node q) {
-        while (p != null && q != null) {
-            if (p.data.equals(q.data)) {
-                p = p.next;
-                q = q.next;
-                continue;
-            } else {
-                return false;
-            }
-        }
-        return true;
-    }
-
     public static void main(String[] args) {
-
+        String[] arrays = {"1","2","2","1"};
         SingleLinkedList linkedList = new SingleLinkedList();
-        String[] a = {"1","1","2","3","2","1","1"};
-        for (int i = 0; i < a.length; i++) {
-            linkedList.insertToTail(a[i]);
+        for (int i = 0; i < arrays.length; i++) {
+            linkedList.insertToTail(arrays[i]);
         }
+        Node p = head;
+        Node q = head;
+        if (q.next!=null && q.next.next!=null){
+            p = p.next;
+            q = q.next.next;
+        }
+        System.out.println("中间元素"+p.data);
+        boolean flag = false;
+        if (q.next == null){
+            System.out.println("链表长度为奇数");
+            Node right = p.next;
+            Node left = flip(p).next;
+            flag = compareNode(right,left);
 
-        System.out.println(linkedList.rang() ? "是回文" : "不是回文");
-
+        }else {
+            Node right = p.next;
+            Node left = flip(p);
+            flag = compareNode(right,left);
+            System.out.println("链表长度为偶数");
+        }
+        if (flag){
+            System.out.println("是回文");
+        }else {
+            System.out.println("不是回文");
+        }
     }
 
 
-    public static class Node {
-        private String data;
-        private Node next;
+
+
+    public class Node {
+        String data;
+        Node next;
 
         public Node(String data, Node next) {
+
             this.data = data;
             this.next = next;
         }
@@ -119,5 +117,4 @@ public class SingleLinkedList {
             this.next = next;
         }
     }
-
 }
